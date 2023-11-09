30ad0e21d4331f4119d4e6fcde14b8963d891e3c
MuriloLucena08 <muriloclucena@gmail.com>
2023-11-08 22:37:14 -0300

    Ultima atualização

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Usando o github");
 turboLogger("Estou aprendendo o fluxo do git");
+turboLogger("Murilo, o Dev, deixou sua contribuição nesse projeto!", "white");
 turboLogger(
     "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema!", "red"
 );
-turboLogger("Murilo, o Dev, deixou sua contribuição nesse projeto!", "white");
