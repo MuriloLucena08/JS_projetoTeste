b38bcb92b3f588bccbee1f6b8c016cf2be9abdc3
MuriloLucena08 <muriloclucena@gmail.com>
2023-11-08 22:06:38 -0300

    Outra pessoa contribuiu para o projeto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { turboLogger } from "./turboLogger.js";
 
 console.log("Usando o github");
-turboLogger("Estou aprendendo o fluxo do git")
+turboLogger("Estou aprendendo o fluxo do git");
+turboLogger("Murilo, o Dev, deixou sua contribuição nesse projeto!", "white");
