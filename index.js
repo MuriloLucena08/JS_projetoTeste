591db8b549d735b06e1327225e6a665ff3b5faa2
MuriloLucena08 <muriloclucena@gmail.com>
2023-11-08 21:40:03 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { turboLogger } from "./turboLogger.js";
 
 console.log("Usando o github");
-turboLogger("Estou aprendendo o fluxo do git")
+turboLogger("Estou aprendendo o fluxo do git");
+turboLogger(
+    "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema!", "red"
+);
